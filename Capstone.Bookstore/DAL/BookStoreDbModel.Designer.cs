4fc0843fc8b08a78760ba307a58e337ca1b21dc3
Neha <neha.deepak.nangia@gmail.com>
2022-04-01 22:08:35 -0400

    Auth feature added

--- Capstone.Bookstore/DAL/BookStoreDbModel.Designer.cs
+++ Capstone.Bookstore/DAL/BookStoreDbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\GIT\Capstone-bookstore\Capstone.Bookstore\DAL\BookStoreDbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Neha\source\repos\Capstone.Bookstore\Capstone.Bookstore\DAL\BookStoreDbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
